8059278e86ca56ea556b728dad803d4d81f0b6af
davidyang2149 <davidyang2149@gmail.com>
2021-07-18 20:22:44 +0900

    :white_check_mark: Add statement TestCase
    - 기본 코드(statement) 테스트 케이스를 추가했습니다.
    - Type tragedy의 관객이 30명 초과인 경우.
    - Type tragedy의 관객이 30명 이하인 경우.
    - Type comedy의 관객이 20명 초과인 경우.
    - Type comedy의 관객이 20명 이하인 경우.
    - Type이 Error인 경우.

--- statement.test.js
+++ statement.test.js
@@ -1,7 +1,7 @@
 const { statement } = require('./statement');
 
 describe('statement', () => {
-  describe('with tragedy', () => {
+  describe('with tragedy & less then 30 audience', () => {
     const invoice = {
       "customer": "BigCo",
       "performances": [
@@ -12,18 +12,121 @@ describe('statement', () => {
         },
       ]
     };
-  
+
     const plays = {
       "hamlet": { "name": "Hamlet", "type": "tragedy" },
     }
 
     it('returns someting', () => {
       const result = statement(invoice, plays);
-  
+
       expect(result).toContain('BigCo');
       expect(result).toContain('Hamlet : $400.00 (1석)');
       expect(result).toContain('총액: $400.00');
       expect(result).toContain('적립 포인트: 0점');
     });
-  })
+  });
+
+  describe('with tragedy & more then 30 audience', () => {
+    const invoice = {
+      "customer": "BigCo",
+      "performances": [
+        {
+          "playID": "hamlet",
+          "audience": 31,
+          "price": 5000
+        },
+      ]
+    };
+
+    const plays = {
+      "hamlet": { "name": "Hamlet", "type": "tragedy" },
+    }
+
+    it('returns someting', () => {
+      const result = statement(invoice, plays);
+
+      expect(result).toContain('BigCo');
+      expect(result).toContain('Hamlet : $410.00 (31석)');
+      expect(result).toContain('총액: $410.00');
+      expect(result).toContain('적립 포인트: 1점');
+    });
+  });
+
+  describe('with comedy & less then 20 audience', () => {
+    const invoice = {
+      "customer": "BigCo",
+      "performances": [
+        {
+          "playID": "as-like",
+          "audience": 1,
+          "price": 5000
+        },
+      ]
+    };
+
+    const plays = {
+      "as-like": { "name": "As You Like It", "type": "comedy" },
+    }
+
+    it('returns someting', () => {
+      const result = statement(invoice, plays);
+
+      expect(result).toContain('BigCo');
+      expect(result).toContain('As You Like It : $303.00 (1석)');
+      expect(result).toContain('총액: $303.00');
+      expect(result).toContain('적립 포인트: 0점');
+    });
+  });
+
+  describe('with comedy & more then 20 audience', () => {
+    const invoice = {
+      "customer": "BigCo",
+      "performances": [
+        {
+          "playID": "as-like",
+          "audience": 21,
+          "price": 5000
+        },
+      ]
+    };
+
+    const plays = {
+      "as-like": { "name": "As You Like It", "type": "comedy" },
+    }
+
+    it('returns someting', () => {
+      const result = statement(invoice, plays);
+
+      expect(result).toContain('BigCo');
+      expect(result).toContain('As You Like It : $468.00 (21석)');
+      expect(result).toContain('총액: $468.00');
+      expect(result).toContain('적립 포인트: 4점');
+    });
+  });
+
+  describe('with unknown', () => {
+    const invoice = {
+      "customer": "BigCo",
+      "performances": [
+        {
+          "playID": "as-like",
+          "audience": 1,
+          "price": 5000
+        },
+      ]
+    };
+
+    const plays = {
+      "as-like": { "name": "As You Like It", "type": "unknown" },
+    }
+
+    it('returns Error', () => {
+      try {
+        statement(invoice, plays);
+      } catch (error) {
+        expect(error).toEqual(new Error('알 수 없는 장르 : unknown'));
+      }
+    });
+  });
 });
